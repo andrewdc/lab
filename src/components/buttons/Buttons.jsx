31b8c0753ab808d4afed6db33be3db3d380090c0
Andrew Colclough <andrew.design@gmail.com>
2015-05-08 15:45:05 -0700

    feat(docs): broke down a couple more modules

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -48,7 +48,9 @@ var Buttons = React.createClass({
   getInitialState: function() {
     return {
       standardButtons: require('raw!./standard-buttons.html'),
-      minimalButtons: require('raw!./minimal-buttons.html')
+      minimalButtons: require('raw!./minimal-buttons.html'),
+      buttonStates: require('raw!./button-states.html'),
+      specialButtons: require('raw!./special-buttons.html')
     };
   },
   render: function () {
@@ -66,27 +68,16 @@ var Buttons = React.createClass({
             title="Minimal Button Styles"
             description="Used for secondary user paths and actions." />
 
-          <div className="docs-panel">
-            <h3>Angular-controlled Button States</h3>
-            <p>These classNamees can be added and removed with js for a responsive and informative user experience.</p>
-            <button className="btn--act is-disabled">is-disabled</button>
-            <button className="btn--act is-successful">is-successful</button>
-            <button className="btn--act is-failed">is-failed</button>
-            <button className="btn--act is-waiting"><i className="fa fa-gear fa-spin"></i> is-waiting</button>
-          </div>
-          <div className="docs-panel">
-            <h3>Special Case Buttons</h3>
-            <p>Where a panel or UI element requires a simple close action (see top right). Typically should cancel whatever action generated the panel.</p>
-            <button className="btn--kill">Kill <i className="fa fa-times"></i></button>
-            <hr/>
-            <p>Up and down (or 'load more') buttons span the width of the container they are in and can be used in modals or embedded on "regular" pages.</p>
-            <button className="btn--more-up"><i className="fa fa-chevron-up"></i>
-            </button>
-            <p className="item">[Content here]</p>
-            <button className="btn--more-down"><i className="fa fa-chevron-down"></i></button>
-            &nbsp;
-            <button className="btn--more-down"><span className="important">Load More</span></button>
-          </div>
+          <Section name="buttonStates"
+            code={this.state.buttonStates}
+            title="Button States"
+            description= "These classes can be added and removed with js for a responsive and informative user experience." />
+
+          <Section name="specialButtons"
+            code={this.state.specialButtons}
+            title="Special Buttons"
+            description=""
+          />
         </section>
       );
   }
